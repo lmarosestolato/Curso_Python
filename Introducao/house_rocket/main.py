40a59815c842b073803efcf05d2e7535d289b64d
lmarosestolato <malurosestolato.lma@gmail.com>
2023-02-13 16:13:15 -0300

    Add comentarios, melhoria de respostas, add reindexacao e reordenacao de dados + localizacao

--- Introducao/house_rocket/main.py
+++ Introducao/house_rocket/main.py
@@ -8,7 +8,26 @@ Responder as perguntas do CEO
 """
 print()
 print("Quantas casas estão disponíveis para compra?")
-print(len(data) - 1, "casas disponíveis.")
+print(len(data['id'].unique()), "casas disponíveis.")
 print()
 print("Quantos atributos as casas possuem?")
-print(len(data.columns), "atributos.")
+print(len(data.drop(['id','date'], axis = 1).columns), "atributos.")
+print()
+print("Quais são os atributos das casas?")
+print("Os atibutos são:", list(data.drop(['id','date'], axis = 1).columns))
+print()
+print("Qual a casa mais cara (casa com o maior valor de venda)?")
+house = data.sort_values('price', ascending=False).reset_index(drop=True)
+print("id:", house.loc[0, 'id'], " valor:", house.loc[0, 'price'])
+print()
+print("Qual a casa com o maior número de quartos?")
+house = data.sort_values('bedrooms', ascending=False).reset_index(drop=True)
+print("id:", house.loc[0, 'id'], " n de quartos:", house.loc[0, 'bedrooms'])
+"""
+
+print()
+
+data.sort_values('bedrooms', ascending=False)
+
+print()
+"""
