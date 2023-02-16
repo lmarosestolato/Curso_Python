96fac8c744309ee49d4eb65208b68bff28f2aa14
lmarosestolato <malurosestolato.lma@gmail.com>
2023-02-16 15:53:55 -0300

    Usando comparativo em .loc

--- Introducao/house_rocket/pt2.py
+++ Introducao/house_rocket/pt2.py
@@ -8,3 +8,7 @@ data['date'] = pd.to_datetime(data['date'])
 print("Qual a data do imóvel mais antigo?")
 house = data.sort_values('date', ascending=True).reset_index(drop=True)
 print("A data registrada mais antiga é", house.loc[0, 'date'])
+print()
+print("Quantos imóveis possuem o número máximo de andares?")
+house = data.loc[data['floors'] == data['floors'].max(), 'floors']
+print(len(house), "imóveis possuem", data['floors'].max(), "andares")
