14b5d8e023775d7312c5b3b74ca158dce73a1f57
lmarosestolato <malurosestolato.lma@gmail.com>
2023-02-15 23:09:44 -0300

    Conversao de var + ordenacao de dados

--- Introducao/house_rocket/pt2.py
+++ Introducao/house_rocket/pt2.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 data = pd.read_csv('datasets/kc_house_data.csv')
 
+#Convertendo as datas de string para date
+data['date'] = pd.to_datetime(data['date'])
+
+print("Qual a data do imóvel mais antigo?")
+house = data.sort_values('date', ascending=True).reset_index(drop=True)
+print("A data registrada mais antiga é", house.loc[0, 'date'])
