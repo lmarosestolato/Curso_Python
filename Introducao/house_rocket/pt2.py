2a1015189e89b3086d0d60c02525a73e2ac7354f
lmarosestolato <malurosestolato.lma@gmail.com>
2023-02-23 11:00:43 -0300

    Geracao de relatorio e mapa dos imoveis

--- Introducao/house_rocket/pt2.py
+++ Introducao/house_rocket/pt2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 data = pd.read_csv('datasets/kc_house_data.csv')
 
@@ -12,3 +13,27 @@ print()
 print("Quantos imóveis possuem o número máximo de andares?")
 house = data.loc[data['floors'] == data['floors'].max(), 'floors']
 print(len(house), "imóveis possuem", data['floors'].max(), "andares")
+print()
+
+#Criacao de um relatorio de nível de padrao dos imóveis
+data['level'] = 'standart'
+data.loc[(data['price'] > 540000), 'level'] = 'high_standart'
+data.loc[(data['price'] <= 540000), 'level'] = 'low_standart'
+
+relatorio = data[['id', 'date', 'price', 'bedrooms', 'sqft_lot', 'level']].sort_values('price')
+relatorio.to_csv('datasets/relatorio_de_padrao.csv', index=False)
+
+#Gerando mapa de localizacao dos imóveis 
+data_map = data[['id', 'lat', 'long', 'price']]
+mapa = px.scatter_mapbox(data_map, lat= 'lat', lon= 'long',
+                  hover_name= 'id', hover_data= ['price'],
+                  color_discrete_sequence= ['blue'],
+                  zoom= 3, height= 300)
+
+mapa.show()
+
+#Melhorando o layout
+mapa.update_layout(mapbox_style = 'open-street-map')
+mapa.update_layout(height = '600', margin = {'r':0, 't':0, 'l':0, 'b':0})
+
+
